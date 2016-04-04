1ed6ef4705a5cce58ac90a3edf63f907c264d5d6
Valentin Zmiycharov <valentin.zmiycharov@gmail.com>
2016-04-04 16:27:57 +0300

    Create maven project with user based recommendations

---
+++ src/main/java/recsys/Main.java
@@ -0,0 +1,41 @@
+package recsys;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+
+import org.apache.mahout.cf.taste.eval.RecommenderBuilder;
+import org.apache.mahout.cf.taste.eval.RecommenderEvaluator;
+import org.apache.mahout.cf.taste.impl.eval.AverageAbsoluteDifferenceRecommenderEvaluator;
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.ThresholdUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.recommender.UserBasedRecommender;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		File file = new File(Main.class.getResource("/ml-1m/ratings.dat").toURI());
+		DataModel model = new FileDataModel(file, "::");
+		
+		UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
+		UserNeighborhood neighborhood = new ThresholdUserNeighborhood(0.1, similarity, model);
+		UserBasedRecommender recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
+		List<RecommendedItem> recommendations = recommender.recommend(2, 3);
+		for (RecommendedItem recommendation : recommendations) {
+		  System.out.println(recommendation);
+		}
+		
+		RecommenderEvaluator evaluator = new AverageAbsoluteDifferenceRecommenderEvaluator();
+		RecommenderBuilder builder = new MyRecommenderBuilder();
+		double result = evaluator.evaluate(builder, null, model, 0.9, 1.0);
+		System.out.println(result);
+	}
+
+}
